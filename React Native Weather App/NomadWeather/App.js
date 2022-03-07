ac36b88818ca4782648498a0f57f90d59528e280
PluxLight <aeoragy@gmail.com>
2022-03-07 22:57:52 +0900

    Update App.js
    날짜 추가

--- React Native Weather App/NomadWeather/App.js
+++ React Native Weather App/NomadWeather/App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
-import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  ActivityIndicator,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 
 const { width: SCREEN_SIZE } = Dimensions.get("window");
 // console.log(SCREEN_SIZE);
 
+const API_KEY = "880f1ef47be871ae5871f72dd7d82145";
+
+const dttoday = (dtvalue) => {
+  let value = new Date(dtvalue * 1000);
+  value = value.getDate();
+
+  return value
+
+};
+
 export default function App() {
   const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -22,9 +40,14 @@ export default function App() {
       { useGoogleMaps: false }
     );
     setCity(location[0].region);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
 
   return (
@@ -38,22 +61,26 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Cloudy</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Rainny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+              <Text style={styles.dayText}>{dttoday(day.dt)}일</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -91,4 +118,12 @@ const styles = StyleSheet.create({
     fontSize: 60,
     color : "white",
   },
+  tinyText: {
+    fontSize: 20,
+    color : "white",
+  },
+  dayText: {
+    color : "white",
+    fontSize : 120,
+  }
 });
